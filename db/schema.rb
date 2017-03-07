1bb76bf9c55cad6a292af267414f40a5c20f824b
Hanyue Zhou <alexalexchou@outlook.com>
2017-03-07 17:07:05 +0200

    pic change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222221303) do
+ActiveRecord::Schema.define(version: 20170307144440) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170222221303) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.integer  "role"
   end
 
 end
