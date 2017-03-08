38c0e2e019b6351a86b49dcfb114b62a3874a99b
Kha Nguyen <nlhkha@gmail.com>
2017-03-08 10:48:29 +0200

    shopping cart

--- app/controllers/services/services_controller.rb
+++ app/controllers/services/services_controller.rb
@@ -7,6 +7,41 @@ class Services::ServicesController < ApplicationController
   def compare
   end
 
+  def add_to_cart
+    # Add item to cart, then redirect
+    add_item_to_cart(params[:id])
+    redirect_to services_cart_url
+  end
+
+  def remove_from_cart
+    remove_item_from_cart(params[:id])
+    redirect_to services_cart_url
+  end
+
+  def cart
+    service_ids = session[:services] || []
+    @services = Service.includes(:supplier).find(service_ids)
+    @subtotal = @services.map { |s| s.price }.sum
+  end
+
   def purchase
+    session[:services] = []
+    redirect_to root_url
+  end
+
+  private
+
+  def add_item_to_cart(service_id)
+    if session[:services].nil?
+      session[:services] = []
+    end
+
+    session[:services].append(service_id).uniq!
+  end
+
+  def remove_item_from_cart(service_id)
+    unless session[:services].nil?
+      session[:services].delete_if { |s| s == service_id}
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
   get '/static_pages/home'
 
   namespace :services do
+    get '/checkout', to: 'services#cart', as: :cart
+    post '/purchase', to: 'services#purchase', as: :purchase
     get '/:id', to: 'services#detail', as: :detail
+    get '/:id/add', to: 'services#add_to_cart', as: :add_to_cart
+    get '/:id/remove', to: 'services#remove_from_cart', as: :remove_from_cart
   end
 
   get '/help', to:'static_pages#help'
