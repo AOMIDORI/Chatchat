1d174e53feb4292e72da6fcd071cd6a124da0d67
Kha Nguyen <nlhkha@gmail.com>
2017-03-08 14:28:09 +0200

    reformat seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ supplier = Supplier.create(
   address: 'Hämeentie 157, 6th floor, H10, FIN-00560 Helsinki',
   phone: '+358 40 774 8085',
   image_url: 'http://sivex-sivusto.etunti.fi/wp-content/uploads/2016/11/sivex_logo_vad-1.png',
-  description: 'We work every day to over 100 unique cleaning . We operate in the Helsinki Metropolitan Area (Helsinki, Espoo, Vantaa, Kauniainen) and its surrounding areas. We serve a broad businesses and private households. Our 450 standard customers can not be wrong.'
+  description: 'We work every day to over 100 unique cleaning. We operate in the Helsinki Metropolitan Area (Helsinki, Espoo, Vantaa, Kauniainen) and its surrounding areas. We serve a broad businesses and private households. Our 450 standard customers can not be wrong.'
 )
 
 services = Service.create([
@@ -23,7 +23,7 @@ services = Service.create([
     image_url: 'http://sivex-sivusto.etunti.fi/wp-content/uploads/2015/09/kotisiivous-e1474623506543.png',
     service_type: :cleaning,
     price: 40,
-    description: 'Vacuuming\r\ndusts wiping\r\nfloor wipe\r\nbeating can carpets\r\nIroning\r\nBed linen exchange\r\nDebris removal\r\nFloor waxing\r\nTerrace and outdoor porch cleaning\r\nCleaning Patio Furniture\r\nCleaning the room and sanitary facilities\r\nWashing windows\r\nHousehold appliances wiping and cleaning'
+    description: "- Vacuuming\r\n- dusts wiping\r\n- floor wipe\r\n- beating can carpets\r\n- Ironing\r\n- Bed linen exchange\r\n- Debris removal\r\n- Floor waxing\r\n- Terrace and outdoor porch cleaning\r\n- Cleaning Patio Furniture\r\n- Cleaning the room and sanitary facilities\r\n- Washing windows\r\n- Household appliances wiping and cleaning"
   },
   {
     supplier: supplier,
@@ -31,7 +31,7 @@ services = Service.create([
     image_url: 'http://sivex-sivusto.etunti.fi/wp-content/uploads/2015/11/muuttosiivouskoira5-e1474280499517.png',
     service_type: :cleaning,
     price: 60,
-    description: 'Window wiping the boards\r\nIkkunasälekaihtimien brushing\r\ncleaning, kitchen appliances, including cooker hood\r\nKitchen cabinets thorough interior and exterior cleaning\r\ncleaning the kitchen tiles\r\nwashing and disinfection of toilet facilities\r\nFloor drain cleaning\r\nthorough cleaning tile surfaces and seams\r\nwashing Sanitary door glass\r\nwashing the sauna walls and benches\r\nWipe the floor surface clammy\r\ncleaning of walls and doors\r\ncleaning of the apartment stains\r\nVacuuming carpets and rugs, and export out of the ventilation'
+    description: "- Window wiping the boards\r\n- Ikkunasälekaihtimien brushing\r\n- cleaning, kitchen appliances, including cooker hood\r\n- Kitchen cabinets thorough interior and exterior cleaning\r\n- cleaning the kitchen tiles\r\n- washing and disinfection of toilet facilities\r\n- Floor drain cleaning\r\n- thorough cleaning tile surfaces and seams\r\n- washing Sanitary door glass\r\n- washing the sauna walls and benches\r\n- Wipe the floor surface clammy\r\n- cleaning of walls and doors\r\n- cleaning of the apartment stains\r\n- Vacuuming carpets and rugs, and export out of the ventilation"
   }
 ])
 
@@ -49,17 +49,13 @@ Review.create([
     user: user,
     date: Time.now,
     rating: 5,
-    comment: 'Exceptional. He worked efficiently from upstairs to downstairs to
-ensure all vents were super clean and that the furnace worked
-properly with no issues.'
+    comment: 'Exceptional. He worked efficiently from upstairs to downstairs to ensure all vents were super clean and that the furnace worked properly with no issues.'
   },
   {
     service: services.second,
     user: user,
     date: Time.now,
     rating: 5,
-    comment: 'David was very professional and clearly an expert in his field. He
-was very thorough and explained the process and what additional
-services were needed. Provided them at very reasonable cost.'
+    comment: 'David was very professional and clearly an expert in his field. He was very thorough and explained the process and what additional services were needed. Provided them at very reasonable cost.'
   }
 ])
